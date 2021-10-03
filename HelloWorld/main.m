eb202db3c86e55ebb4ac3595c36cd9f53858b903
NSJ <nsj0409@gmail.com>
2021-10-03 11:33:58 +0900

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by metanet on 2021/10/03.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
